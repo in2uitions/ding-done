94d837f33582add0f5804480afdaba5f7c217c50
in2uitions <joseph.mhanna@in2uitions.com>
2024-06-14 16:24:54 +0300

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+const apiKey = "fd60d15761fc72569cf0be529c5666557c5b7230";
+const appId = "2307408436e8a304";
+const appAuth = "9414e2161e5f2d84138ebefd60a02c1062610ca5";
+const region = "eu";
+const jobSeeker = "66bf2dcf-fa19-4285-9ab4-9a7dda108925";
+const recruiter = "5cfe84cc-26b3-4880-b770-90df33b2142e";
+const cometChatUrl = "https://$appId.api-$region.cometchat.io/v3/users";
+
+const double defaultPadding = 16.0;
