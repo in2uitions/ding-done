5ffe096ac079f85d784c43bdf6b43ea865d410d3
in2uitions <joseph.mhanna@in2uitions.com>
2024-06-14 16:05:23 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/dingdone/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.in2uitions.dingdone
+
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
