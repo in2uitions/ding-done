2deb67325f8d49416521ce95b48194f9d0785a14
in2uitions <joseph.mhanna@in2uitions.com>
2024-10-10 14:29:12 +0300

    .

--- android/app/src/main/kotlin/com/example/dingdone/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.in2uitions.dingdone
-
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
